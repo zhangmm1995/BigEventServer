753f3ea6330e1753c5eab9add6c55466ae298e9c
zhangmm1995 <1003805945@qq.com>
2020-11-11 11:10:51 +0800

    获取用户基本信息接口配置完成

--- app.js
+++ app.js
@@ -2,8 +2,23 @@ const path = require('path');
 const express = require('express');
 const app = express();
 const cors = require('cors');
+const expressJWT = require('express-jwt');
 
-
+// unless ： 如果没有 unless方法，则默认所有接口都需要验证，unless把不需要验证的接口排除掉。
+/* 
+secret : 加密秘钥
+algorithms ： 加密算法 ['HS256']
+path 取值范围：
+  字符串：表示排除一个地址 如 '/api/login'
+  数组： 表示排除多个地址 如 ['/api/login', ]
+  正则： 表示排除符合规则的地址 如 /^\/api/  (排除以 /api开头的接口)
+*/
+// （此方法写在静态资源加载之后，不然静态资源不能访问）
+app.use(
+  expressJWT({ secret: 'bigevent-9760', algorithms: ['HS256'] }).unless({
+    path: /^\/api/,
+  })
+);
 
 // 配置服务器
 app.listen(8001, () => console.log('大事件服务器启动'));
@@ -14,5 +29,8 @@ app.use(cors());
 // 设置获取post请求体参数
 app.use(express.urlencoded({ extended: false }));
 
-// 使用注册路由
+// 使用注册登录路由
 app.use('/api', require(path.join(__dirname, 'routers', 'login')));
+
+// 使用个人中心路由
+app.use('/my', require(path.join(__dirname, 'routers', 'user')));
--- routers/user.js
+++ routers/user.js
@@ -1,9 +1,28 @@
+const utility = require('utility');
 const express = require('express');
 const router = express.Router();
 // 引用数据库
 const database = require('../database');
 
+// 获取用户的基本信息路由
+router.get('/userinfo', async (req, res) => {
+  let username = req.user.username;
+  let value = await database('select * from user where username=?', username);
+  if (value && value.length > 0) {
+    res.json({
+      status: 0,
+      message: '获取用户基本信息成功！',
+      data: value[0],
+    });
+  } else {
+    res.json({
+      status: 1,
+      message: '获取用户基本信息失败！',
+    });
+  }
+});
 
+// 
 
 // 导出路由
 module.exports = router;
